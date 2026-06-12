d33ff1cf5d753a8edc7928ef7d326187c2d4df20
Isaiah Rodriguez <irod196@wgu.edu>
2026-06-12 17:43:31 -0500

    Create Server and used port 8080 , added a response of Hello World, console req url and method.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // write your code here
+const http = require('http');
+
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+
+    console.log(req.url, req.method);
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World');
+
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
